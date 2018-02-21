eeace29ff04d1ec98a11efea6476108ac3299388
julientregoat <julienctregoat@gmail.com>
2018-02-20 23:50:12 -0500

    saving progress, removed filter logic from filter to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,26 @@ import React from 'react';
 
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+class App extends React.Component {
+  state = {
+    fruit: [],
+    filters: [],
+    currentFilter: null
+  }
+  
+  componentWillMount() {
+    this.fetchFilters();
+  }
+  
+  fetchFilters = () => {
+    fetch('/api/fruit_types')
+      .then(response => response.json())
+      .then(filters => this.setState({ filters }));
+  }
+  
+  render(){
+    return <FruitBasket filters={this.state.filters}/>
+  }
+}
 
 export default App;
--- src/components/FruitBasket.js
+++ src/components/FruitBasket.js
@@ -21,12 +21,20 @@ class FruitBasket extends Component {
   render() {
     return (
       <div className="fruit-basket">
-        <Filter handleChange={this.handleFilterChange} />
+        <Filter
+        filters={this.props.filters} handleChange={this.handleFilterChange} />
         <FilteredFruitList
           filter={this.state.selectedFilter} />
       </div>
     );
   }
+}
+
+FruitBasket.defaultProps = {
+  fruit: "",
+  filters: "",
+  currentFilter: "",
+  updateFilterCallback: ""
 }
 
 export default FruitBasket;
